47e02196bcaf400d2735a311dd8c2562b23f1b0b
Aitor1995 <aitor1995@openmailbox.org>
2015-03-11 13:53:45 +0100

    Fondo records

--- app/src/main/java/com/aitor1995/galactic_journey/activities/RecordsActivity.java
+++ app/src/main/java/com/aitor1995/galactic_journey/activities/RecordsActivity.java
@@ -2,11 +2,13 @@ package com.aitor1995.galactic_journey.activities;
 
 import android.os.Bundle;
 
+import com.aitor1995.galactic_journey.R;
+
 public class RecordsActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        getWindow().setBackgroundDrawableResource(R.drawable.background);
     }
 }
